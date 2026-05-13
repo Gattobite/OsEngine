10f93044354017efd721412e8af4fc68084d7884
Gattobite <tsiunels@hotmail.com>
2026-05-13 16:46:36 +0400

    new bot to the build

---
+++ project/OsEngine/Robots/NewsBots/TestBotServer.cs
@@ -0,0 +1,36 @@
+﻿using OsEngine.Entity;
+using OsEngine.Language;
+using OsEngine.OsTrader.Panels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OsEngine.Robots.NewsBots
+{
+    public class TestBotServer : BotPanel
+    {
+        public TestBotServer(string name, StartProgram startProgram)
+            : base(name, startProgram)
+        {
+            // Create tabs
+            TabCreate(BotTabType.Simple);
+
+            Description = OsLocalization.Description.DescriptionLabel28;
+        }
+
+        // The name of the robot in OsEngine
+        public override string GetNameStrategyType()
+        {
+            return "TestBotServer";
+        }
+
+        // Show settings GUI
+        public override void ShowIndividualSettingsDialog()
+        {
+            MessageBox.Show(OsLocalization.Trader.Label57);
+        }
+    }
+}
